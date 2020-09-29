86ef700e28776aa8434261f15d2a5bc221f29288
Dirac <1063238990@qq.com>
2020-09-29 23:11:16 +0800

    [ADD]增加了He llo world语 句

--- demo/src/com/company/Main.java
+++ demo/src/com/company/Main.java
@@ -2,7 +2,8 @@ package com.company;
 
 public class Main {
 
+    Math.pow
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World!");
     }
 }
